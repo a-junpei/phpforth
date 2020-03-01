ee1edfd48dc9b83343cc3a35a4c36409f7540218
Arikawa Junpei <junpei.1982@gmail.com>
2020-03-02 01:04:14 +0900

    ワード定義ひとまずシンプル版として実装

--- forth.php
+++ forth.php
@@ -1,8 +1,5 @@
 <?php
 
-// $str = "10 20 + .";
-// eval_str($str);
-
 test_eval_str("10 20 + .", "30");
 test_eval_str("30 20 - .", "10");
 test_eval_str("10 20 * .", "200");
@@ -10,6 +7,8 @@ test_eval_str("200 20 / .", "10");
 test_eval_str("10 30 + 30 20 - * 40 / .", "10");
 test_eval_str("10 20 30 + 5 + * .", "550");
 test_eval_str("65 66 emit", "B");
+test_eval_str("2 dup * .", "4");
+test_eval_str(": cube dup dup * * ; 4 cube .", "64");
 
 function eval_str($str) {
     $tokens = explode(' ', $str);
@@ -48,20 +47,48 @@ function eval_str($str) {
             $result = $x / $y;
             array_push($stack, $result);
         }, 
+        'dup' => function() use (&$stack) {
+            $x = array_pop($stack);
+            array_push($stack, $x);
+            array_push($stack, $x);
+        }, 
         'emit' => function() use (&$stack) {
             $x = array_pop($stack);
             echo chr($x);
         }, 
     ];
+
+    $definition_mode = false;
+    $definition_stack = [];
+    
     foreach ($tokens as $key => $token) {
         // echo "[debug]({$key}) : token='{$token}' stack=" . print_r($stack, true) . PHP_EOL;
-    
-        if (isset($words[$token])) {
-            $words[$token]();
-        } else if (is_numeric($token)) {
-            array_push($stack, $token);
+
+        if ($definition_mode) {
+            if ($token === ';') {
+                $name = array_shift($definition_stack);
+                $definitions = $definition_stack;
+                $words[$name] = function() use (&$stack, $words, $definitions) {
+                    foreach($definitions as $definition) {
+                        $words[$definition]();
+                    }
+                };
+
+                $definition_mode = false;
+                $definition_stack = [];
+            } else {
+                array_push($definition_stack, $token);
+            }
         } else {
-            ;
+            if ($token === ':') {
+                $definition_mode = true;
+            } else if (isset($words[$token])) {
+                $words[$token]();
+            } else if (is_numeric($token)) {
+                array_push($stack, $token);
+            } else {
+                ;
+            }
         }
     }
 }
